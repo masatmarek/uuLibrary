db56e530bb0133445d67dbb576810423e8484997
Šimon Petráček <simonpetracekk@gmail.com>
2020-05-27 17:23:19 +0200

    added new features update book modal etc

--- uu_library_maing01-hi/src/core/update-book-modal-lsi.js
+++ uu_library_maing01-hi/src/core/update-book-modal-lsi.js
@@ -13,7 +13,12 @@ let Lsi = {
   genrePlaceHolder: { cs: "Vyberte alespoň jeden žánr", en: "Pick at least one genre." },
   conditionLabel: { cs: "Kondice", en: "Location" },
   modalHelpUpdate: { cs: "Tento modál slouží pro vytvoření knihy.", en: "Use this modal to update book." },
-  modalHeaderUpdate: { cs: "Upravit knihu", en: "Update Book" }
+  modalHeaderUpdate: { cs: "Upravit knihu", en: "Update Book" },
+  publisher: { cs: "Vydavatel", en: "Publisher" },
+  dateOfPublication: { cs: "Datum vydání", en: "Date of publication" },
+  language: { cs: "Jazyk", en: "Language" },
+  custody: { cs: "Vazba", en: "Custody" },
+  numberOfPages: { cs: "Počet stran", en: "Number of pages" }
 };
 
 export default Lsi;
